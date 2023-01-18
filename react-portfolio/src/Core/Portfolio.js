c8ecb1254ce23de1f04b612842f65fa0bcfc2118
Matthew Erwin <matthewderwin23@gmail.com>
2023-01-18 00:27:48 -0600

    done but now I just need to make it responsive

--- react-portfolio/src/Core/Portfolio.js
+++ react-portfolio/src/Core/Portfolio.js
@@ -4,41 +4,87 @@ import "./Portfolio.css"
 
 function Portfolio() {
 
-    //do a simple grid like in mw2 and at the bottom have a div that gives us a link to how I made this site.
-    //Title it like How I made this site? {link}
+    const [popup, setPopup] = useState({})
+    
+    const [popupWord, setPopupWord] = useState({})
 
-    const [popup, setPopup] = useState({
-        backgroundImage: "../pictures/fithappens.JPG",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        cursor: 'pointer'
+    const [popupPort, setPopupPort] = useState({})
+
+    const [fitText, setFitText] = useState({
+        opacity: '0'
     })
 
+    const [wordText, setWordText] = useState({
+        opacity: '0'
+    })
+
+    const [portText, setPortText] = useState({
+        opacity: '0'
+    })
 
     function handleClick() {
+
         setPopup({
             backgroundImage: 'none',
-            backgroundColor: '#ff0000',
-            cursor: 'pointer'
+            backgroundColor: '#3f203b',
+            cursor: 'pointer',
+            animation: 'none'
+        })
+
+        setFitText({
+            opacity: '100'
         })
     }
 
     function closePop() {
-        setPopup(prev => {
-            return {
-                ...prev
-            }
+        setPopup({animation: 'none'})
+        setFitText({
+            opacity: '0'
         })
     }
 
-    // function closePop() {
-    //     setPopup(prev => {
-    //         return {
-    //             ...prev
-    //         }
-    //     })
-    // }
-    
+    function handleClickWord() {
+
+        setPopupWord({
+            backgroundImage: 'none',
+            backgroundColor: '#3f203b',
+            cursor: 'pointer',
+            animation: 'none'
+        })
+
+        setWordText({
+            opacity: '100'
+        })
+    }
+
+    function closePopWord() {
+        setPopupWord({animation: 'none'})
+        setWordText({
+            opacity: '0'
+        })
+    }
+
+    function handleClickPort() {
+
+        setPopupPort({
+            backgroundImage: 'none',
+            backgroundColor: '#3f203b',
+            cursor: 'pointer',
+            animation: 'none'
+        })
+
+        setPortText({
+            opacity: '100'
+        })
+    }
+
+    function closePopPort() {
+        setPopupPort({animation: 'none'})
+        setPortText({
+            opacity: '0'
+        })
+    }
+
 
     return (
         <div className="port-box">
@@ -53,9 +99,29 @@ function Portfolio() {
             
             <div className="port-container-grid">
     
-                <div className="Fithappens" onClick={handleClick} onMouseLeave={closePop} style={popup}></div>
-                <div className="WordJustWord"></div>
-                <div className="Portfolio"></div>
+                <div className="Fithappens" onClick={handleClick} onMouseLeave={closePop} style={popup}>
+                    <div style={fitText}>
+                        <p>This was a group project, and this website allows users to post/edit/delete their own workouts and make their own accounts to do so. I did a lot of work on the frontend of this project.</p>
+                        <p>Tools: ASP.NET Core, React, MSSQL, Apache</p>
+                        <a href='https://github.com/matterwin/csc3380'><button  className='fit-button'>Github</button></a>
+                    </div>
+                </div>
+                <div className="WordJustWord" onClick={handleClickWord} onMouseLeave={closePopWord} style={popupWord}>
+                    <div style={wordText}>
+                        <p>I created a forum based website, like a mini "reddit", where I used MySQL to store all of the users' data. This however wasn't a completed project.</p>
+                        <p>Tools: Java, Java Servlets, HTML/CSS/JS, MySQL, Apache</p>
+                        <a href='https://github.com/matterwin/wordjustwordWebsite'><button  className='fit-button'>Github</button></a>
+                        <a href='https://youtu.be/pkejclj3zN4'>&nbsp;<button  className='fit-button'>YouTube</button></a>
+                    </div>
+                </div>
+                <div className="Portfolio" onClick={handleClickPort} onMouseLeave={closePopPort} style={popupPort}>
+                    <div style={portText}>
+                        <p>Well, you're on my site right now. The SS says "portfolio," but I just didn't have a better SS to put up here. Any questions, just contact me.</p>
+                        <p>Tools: React, CSS</p>
+                        <a href='https://github.com/matterwin/portfolio'><button  className='fit-button'>Github</button></a>
+                    </div>
+                </div>
+
             </div>
         </div>
             
