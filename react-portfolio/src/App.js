fdea935f0936558a7b6a3b1c9bd92ab247604729
Matthew Erwin <matthewderwin23@gmail.com>
2023-01-17 21:19:10 -0600

    hghg

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -15,27 +15,27 @@ function App() {
 
   //maybe do a weather rain that shows the weather of my location
 
-  const createCursor = (x,y) => {
-    const cursor = document.createElement('div');
-    cursor.className = 'cursor';
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-    return cursor;
-}
+  // const createCursor = (x,y) => {
+  //   const cursor = document.createElement('div');
+  //   cursor.className = 'cursor';
+  //   cursor.style.left = x + 'px';
+  //   cursor.style.top = y + 'px';
+  //   return cursor;
+  // }
 
-const removerCursor = cursor => {
-    const timeout = setTimeout(() => {
-        cursor.remove();
-        clearTimeout(timeout);
-    }, 1000)
-}
+  // const removerCursor = cursor => {
+  //     const timeout = setTimeout(() => {
+  //         cursor.remove();
+  //         clearTimeout(timeout);
+  //     }, 1000)
+  // }
 
-window.addEventListener('click', e => {
-    const cursor = createCursor(e.pageX, e.pageY);
-    document.body.appendChild(cursor);
-    removerCursor(cursor);
-    console.log(e.pageX, e.pageY);
-})
+  // window.addEventListener('click', e => {
+  //     const cursor = createCursor(e.pageX, e.pageY);
+  //     document.body.appendChild(cursor);
+  //     removerCursor(cursor);
+  //     console.log(e.pageX, e.pageY);
+  // })
 
   return (
     <div>
--- react-portfolio/src/Core/Portfolio.js
+++ react-portfolio/src/Core/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 import "./Portfolio.css"
 
@@ -7,15 +7,58 @@ function Portfolio() {
     //do a simple grid like in mw2 and at the bottom have a div that gives us a link to how I made this site.
     //Title it like How I made this site? {link}
 
+    const [popup, setPopup] = useState({
+        backgroundImage: "../pictures/fithappens.JPG",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        cursor: 'pointer'
+    })
+
+
+    function handleClick() {
+        setPopup({
+            backgroundImage: 'none',
+            backgroundColor: '#ff0000',
+            cursor: 'pointer'
+        })
+    }
+
+    function closePop() {
+        setPopup(prev => {
+            return {
+                ...prev
+            }
+        })
+    }
+
+    // function closePop() {
+    //     setPopup(prev => {
+    //         return {
+    //             ...prev
+    //         }
+    //     })
+    // }
+    
+
     return (
         <div className="port-box">
-            <h1 className="about">My Collection</h1>
+            <div>
+                <h1 className="about">My Collection</h1>
                 <div className="special">
-                        <h1 className="block1">&nbsp;</h1>
-                        <h1 className="block">&nbsp;</h1>
-                        <h1 className="block1">&nbsp;</h1>
+                    <h1 className="block1">&nbsp;</h1>
+                    <h1 className="block">&nbsp;</h1>
+                    <h1 className="block1">&nbsp;</h1>
+                </div>
+            </div>
+            
+            <div className="port-container-grid">
+    
+                <div className="Fithappens" onClick={handleClick} onMouseLeave={closePop} style={popup}></div>
+                <div className="WordJustWord"></div>
+                <div className="Portfolio"></div>
             </div>
         </div>
+            
     )
 }
 
